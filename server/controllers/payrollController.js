78adbfe924dc288b1374d1e8ff95578653aacf65
Divya Mondal <divyamondal2001@gmail.com>
2026-07-11 12:53:12 +0530

    puppeteer sandbox removed

--- server/controllers/payrollController.js
+++ server/controllers/payrollController.js
@@ -717,10 +717,13 @@ Authorized Signatory
 </html>
 `;
 
-    const browser =
-      await puppeteer.launch({
-        headless: true,
-      });
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+  ],
+});
 
     const page =
       await browser.newPage();
@@ -1187,11 +1190,13 @@ Authorized Signatory
 </html>
 `;
 
-    const browser =
-      await puppeteer.launch({
-        headless: true,
-      });
-
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+  ],
+});
     const page =
       await browser.newPage();
 
